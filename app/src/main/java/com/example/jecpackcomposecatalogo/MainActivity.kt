36d98be7cfa19db548a4d8bf020e0c1bcff07363
erixjavier <Mendhoza123#>
2023-02-03 11:55:41 -0500

    componentes de Text con compose

--- app/src/main/java/com/example/jecpackcomposecatalogo/MainActivity.kt
+++ app/src/main/java/com/example/jecpackcomposecatalogo/MainActivity.kt
@@ -20,8 +20,13 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jecpackcomposecatalogo.ui.theme.JecpackComposeCatalogoTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +39,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MyStateExample()
+                    MyText()
                 }
             }
         }
@@ -151,10 +156,30 @@ fun MyStateExample(){
     }
 }
 
+
+
+@Composable
+fun MyText(){
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "This is an example")
+        Text(text = "This is an example", color = Color.Red)
+        Text(text = "This is an example", fontWeight = FontWeight.ExtraBold)
+        Text(text = "This is an example", fontWeight = FontWeight.Light)
+        Text(text = "This is an example", style = TextStyle(fontFamily = FontFamily.Cursive))
+        Text(text = "This is an example", style = TextStyle(textDecoration = TextDecoration.LineThrough))
+        Text(text = "This is an example", style = TextStyle(textDecoration = TextDecoration.Underline))
+        Text(text = "This is an example", style = TextStyle(textDecoration = TextDecoration.combine(
+            listOf(TextDecoration.LineThrough,TextDecoration.Underline)
+        )))
+        Text(text = "This is an example", fontSize = 30.sp)
+    }
+
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JecpackComposeCatalogoTheme {
-        MyStateExample()
+        MyText()
     }
 }
