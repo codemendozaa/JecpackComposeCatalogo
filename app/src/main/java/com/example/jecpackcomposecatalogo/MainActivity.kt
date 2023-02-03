6315a50e34aed49864e7d55779476e64b3866f21
erixjavier <Mendhoza123#>
2023-02-03 10:53:59 -0500

    MyStateExample

--- app/src/main/java/com/example/jecpackcomposecatalogo/MainActivity.kt
+++ app/src/main/java/com/example/jecpackcomposecatalogo/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MyComplexLayout()
+                    MyStateExample()
                 }
             }
         }
@@ -133,10 +136,22 @@ fun MyBox() {
     }
 }
 
+@Composable
+fun MyStateExample(){
+    var counter  = remember{mutableStateOf(0)}
+    Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Button( onClick = { counter.value +=1 }) {
+            Text(text = "Click")
+        }
+        Text(text = "He sido pulsado ${counter.value} veces")
+        
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JecpackComposeCatalogoTheme {
-        MyComplexLayout()
+        MyStateExample()
     }
 }
